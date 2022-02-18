874f72c1cb9e92563f7667622f5a3bde9c6c21a7
Jailton gilberto <86026307+JailtonYagami@users.noreply.github.com>
2022-02-18 18:51:02 -0300

    Mundando no github

--- java-fundamentos/src/Aula01.java
+++ java-fundamentos/src/Aula01.java
@@ -2,5 +2,7 @@ public class Aula01 {
     public static void main(String[] args) {
 
         System.out.println("Hellow Word!!!");
+        System.out.println("Agora simmmmm!!!");
+        
     }
 }
